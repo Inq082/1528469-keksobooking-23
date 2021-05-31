26b38b4c5c8afb14635300a3a653d598385b96e2
Inq082 <smail_2001@rambler.ru>
2021-05-31 18:40:55 +0300

    Update main.js
    2.14 fix

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 function getRandomBetween(min,max){
   return Math.floor(Math.random()*(max-min+1)+min);
 }
+getRandomBetween(2,10);//temporary
 //убираем округление и добавляем к параметрам условие указания количество знаков после запятой
-function getRandomNumberBetween(min,max,point){
+function getRandomNumberPoint(min,max,point){
 
   return (Math.random()*(max-min+1)+min).toFixed (point);
 }
+getRandomNumberPoint(2,10,4);//temporary
