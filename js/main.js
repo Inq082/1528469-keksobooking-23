ad1ba15ef932d663e3c211c8258deb066c54a521
Inq082 <smail_2001@rambler.ru>
2021-06-08 11:44:11 +0300

    Update main.js
    стилистика

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //Округляем до целого,max и min включаются
 function getRandomBetween(min,max){
-  return Math.floor(Math.random()*(max-min+1)+min);
+  return Math.floor(Math.random() * (max-min + 1)+min);
 }
 getRandomBetween(2,10);//temporary
 //убираем округление и добавляем к параметрам условие указания количество знаков после запятой
 function getRandomNumberPoint(min,max,point){
 
-  return (Math.random()*(max-min+1)+min).toFixed (point);
+  return (Math.random() * (max-min + 1)+min).toFixed (point);
 }
 getRandomNumberPoint(2,10,4);//temporary
