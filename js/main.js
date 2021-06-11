f48df5d373dc7e0461cd99148825877ae9c63ace
Inq082 <smail_2001@rambler.ru>
2021-06-11 16:18:52 +0300

    4.9
     Больше деталей

--- js/main.js
+++ js/main.js
@@ -9,3 +9,71 @@ function getRandomNumberPoint(min, max, point){
   return (Math.random() * (max - min + 1) + min).toFixed(point);
 }
 getRandomNumberPoint(2, 10, 4);//temporary
+//Генерируем рандомный массив заданной длины
+let getArrayRandomLength = function (array, length) {
+  let shufleArray = array.slice();
+  let temporaryValue;
+  let randomIndex;
+
+  for (let currentIndex = shufleArray.length - 1; currentIndex > 0; currentIndex--) {
+    randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+    randomIndex = getRandomBetween(0, currentIndex);
+    temporaryValue = shufleArray[currentIndex];
+    shufleArray[currentIndex] = shufleArray[randomIndex];
+    shufleArray[randomIndex] = temporaryValue;
+  }
+  shufleArray.length = length;
+  return shufleArray;
+}
+
+let offerTitle = [
+  'Большой дворец',
+  'Небольшой дворец',
+  'Огромная квартира',
+  'Маленькая квартира',
+  'Красивый домик',
+  'Некрасивый домик',
+  'Уютное бунгало',
+  'Неуютное бунгало',
+  'Апарт отель',
+  'Хостел'
+];
+let offerType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+let offerCheckIn = ['12:00', '13:00', '14:00'];
+let offerCheckOut = ['12:00', '13:00', '14:00'];
+let offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+let offerPhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+// Генерируем объект
+let createObject = function(i) {
+  let objectRoom = {
+    author: {
+      avatar: 'img/avatars/user' + (i < 10 ? '0' : '') + (i + 1) + '.png'
+    },
+    'offer': {
+      'title': offerTitle[i],
+      'address': getRandomNumberPoint(35.65000, 35.70000, 5) + ', ' + getRandomNumberPoint(139.70000 , 139.80000, 5),
+      'price': getRandomBetween(100, 1000000),
+      'type': offerType[getRandomBetween(0, offerType.length - 1)],
+      'rooms': getRandomBetween(1, 7),
+      'guests': getRandomBetween(1, 20),
+      'checkin': offerCheckIn[getRandomBetween(0, offerCheckIn.length - 1)],
+      'checkout': offerCheckOut[getRandomBetween(0, offerCheckOut.length - 1)],
+      // Случайное кол-во, случаные значения, не должны повторяться
+      'features': getArrayRandomLength(offerFeatures, getRandomBetween(1, offerFeatures.length - 1)),
+      'description': '',
+      // Случайная длина
+      'photos': getArrayRandomLength(offerPhotos, offerPhotos.length)
+    },
+
+    'location': {
+      'x': getRandomNumberPoint(35.65000, 35.70000, 5),
+      'y': getRandomNumberPoint(139.70000 , 139.80000, 5),
+    }
+  };
+
+  return objectRoom;
+}
