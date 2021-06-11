e696a8c1b4c62f7a5f1e355497902244f2ff5571
Inq082 <smail_2001@rambler.ru>
2021-06-11 22:23:14 +0300

    4.9

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+
+const QUANTITYOFFERS = 10;
+const OFFERTITLE = [
+  'Большой дворец',
+  'Небольшой дворец',
+  'Огромная квартира',
+  'Маленькая квартира',
+  'Красивый домик',
+  'Некрасивый домик',
+  'Уютное бунгало',
+  'Неуютное бунгало',
+  'Апарт отель',
+  'Хостел',
+];
+const OFFERTYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const OFFERCHECKIN = ['12:00', '13:00', '14:00'];
+const OFFERCHECKOUT = ['12:00', '13:00', '14:00'];
+const OFFERFEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const OFFERPHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 //Округляем до целого,max и min включаются
 function getRandomBetween(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -25,48 +48,26 @@ function getArrayRandomLength(array, length) {
   shufleArray.length = length;
   return shufleArray;
 }
-
-const offerTitle = [
-  'Большой дворец',
-  'Небольшой дворец',
-  'Огромная квартира',
-  'Маленькая квартира',
-  'Красивый домик',
-  'Некрасивый домик',
-  'Уютное бунгало',
-  'Неуютное бунгало',
-  'Апарт отель',
-  'Хостел',
-];
-const offerType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const offerCheckIn = ['12:00', '13:00', '14:00'];
-const offerCheckOut = ['12:00', '13:00', '14:00'];
-const offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const offerPhotos = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
 // Генерируем объект
-const createObject = function (index) {
+const createObject = function(index) {
   const objectRoom = {
     author: {
       avatar: `img/avatars/user ${index < 10 ? '0' : ''} ${index + 1} $.png`,
     },
     offer : {
-      title : offerTitle[index],
-      address : `getRandomNumberPoint(35.65000, 35.70000, 5) ${', '} ${getRandomNumberPoint(139.70000 , 139.80000, 5)}`,
+      title : OFFERTITLE[index],
+      address : `${getRandomNumberPoint(35.65000, 35.70000, 5)} , ${getRandomNumberPoint(139.70000 , 139.80000, 5)}`,
       price : getRandomBetween(100, 1000000),
-      type : offerType[getRandomBetween(0, offerType.length - 1)],
+      type : OFFERTYPE[getRandomBetween(0, OFFERTYPE.length - 1)],
       rooms : getRandomBetween(1, 7),
       guests : getRandomBetween(1, 20),
-      checkin : offerCheckIn[getRandomBetween(0, offerCheckIn.length - 1)],
-      checkout : offerCheckOut[getRandomBetween(0, offerCheckOut.length - 1)],
+      checkin : OFFERCHECKIN[getRandomBetween(0, OFFERCHECKIN.length - 1)],
+      checkout : OFFERCHECKOUT[getRandomBetween(0, OFFERCHECKOUT.length - 1)],
       // Случайное кол-во, случаные значения, не должны повторяться
-      features : getArrayRandomLength(offerFeatures, getRandomBetween(1, offerFeatures.length - 1)),
+      features : getArrayRandomLength(OFFERFEATURES, getRandomBetween(1, OFFERFEATURES.length - 1)),
       description : '',
       // Случайная длина
-      photos : getArrayRandomLength(offerPhotos, offerPhotos.length),
+      photos : getArrayRandomLength(OFFERPHOTOS, OFFERPHOTOS.length),
     },
 
     location: {
@@ -76,3 +77,12 @@ const createObject = function (index) {
   };
   return objectRoom;
 };
+function createListOffers(quantityElements) {
+  const listElement = [];
+  for (let i = 0; i < quantityElements; i++) {
+    listElement[i] = createObject(i);
+  }
+  return listElement;
+}
+const listOffers = createListOffers(QUANTITYOFFERS);
+
