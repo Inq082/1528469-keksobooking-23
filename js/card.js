b6c5dc764b7f036cf8c730f64489c8a7c9b76191
Inq082 <smail_2001@rambler.ru>
2021-06-21 09:29:59 +0300

    7.9

--- js/card.js
+++ js/card.js
@@ -27,23 +27,6 @@ const setPhotosElement = (photoadsList, container) => {
     container.classList.add('hidden');
   }
 };
-// отрисовка features
-const genFeatures = function (newFeature) {
-  const futureContainer = document.querySelector('.popup__features');
-  futureContainer.innerHTML = '';
-
-  if (newFeature === 0) {
-    futureContainer.classList.add('hidden');
-  } else {
-    newFeature.forEach ((feature) => {
-      const li = document.createElement('li');
-      li.classList.add(`.popup__feature, .popup__feature--${feature}`);
-      futureContainer.appendChild(li);
-    });
-    return futureContainer;
-  }
-};
-
 //создаем DOM объект
 export const getCardTemplate = (adsItem) => {
   const cardTemplate = document.querySelector('#card')
@@ -61,8 +44,23 @@ export const getCardTemplate = (adsItem) => {
   setCardElementText(cardElement, '.popup__text--capacity', `${adsItem.offer.rooms} ${rooms} для ${adsItem.offer.guests} ${guests}`);
   setCardElementText(cardElement, '.popup__text--time', `Заезд после ${adsItem.offer.checkin} , выезд до ${adsItem.offer.checkout}`);
   //setCardElementText(cardElement, '.popup__features', adsItem.offer.features.join(', '));
-  cardElement.querySelector('.popup__features').replaceWith(genFeatures(adsItem));
-
+  // отрисовка features
+  const genFeatures = function (newFeature) {
+    const futureContainer = cardElement.querySelector('.popup__features');
+    const feature = adsItem.offer.features;
+    if (newFeature === 0) {
+      futureContainer.classList.add('hidden');
+    } else {
+      futureContainer.innerHTML = '';
+      if (adsItem.offer.features.length > 0) {
+        const li = document.createElement('li');
+        li.classList.add('popup__feature');
+        li.classList.add(`popup__feature--${feature}`);
+        futureContainer.appendChild(li);
+      }
+    }
+    return futureContainer;
+  };
   /*const futureContainer = cardElement.querySelector('.popup__features');
   if (adsItem.offer.features.length > 0) {
     futureContainer.innerHTML = '';
