461b6e2d155c7458b1cc9e11eef115689e6d20cb
Inq082 <smail_2001@rambler.ru>
2021-06-27 14:39:44 +0300

    8.10.1

--- js/form.js
+++ js/form.js
@@ -5,6 +5,8 @@ const offerPrice = offerForm.querySelector('#price');
 const capacitySelect = offerForm.querySelector('#capacity');
 const selectRooms = offerForm.querySelector('#room_number');
 const offerTime = offerForm.querySelector('.ad-form__element--time');
+const timeIn = offerTime.querySelector('#timein');
+const timeOut = offerTime.querySelector('#timeout');
 
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
@@ -48,31 +50,6 @@ const checkRoomNumberCapacityValidity = () => {
 
 };
 
-/*const checkTypeValidity = () => {
-  switch (offerType.value) {
-    case 'bungalow':
-      MIN_PRICE = 0;
-      break;
-    case 'flat':
-      MIN_PRICE = 1000;
-      break;
-    case 'hotel':
-      MIN_PRICE = 3000;
-      break;
-    case 'house':
-      MIN_PRICE = 5000;
-      break;
-    case 'palace':
-      MIN_PRICE = 10000;
-      break;
-    default:
-      MIN_PRICE = 1000;
-  }
-
-  offerPrice.placeholder = MIN_PRICE;
-};
-
- */
 // Валидация цены
 
 const checkPriceValidity = () => {
@@ -87,37 +64,25 @@ const checkPriceValidity = () => {
   }
   offerPrice.placeholder = DEFAULT_MIN_PRICE[offerType.value];
   offerPrice.reportValidity();
-  //checkTypeValidity();
 };
 
 // Синхронизация полей времени заезда и выезда
-const checkTimeValidity = (evt) => {
-  const timeIn = offerTime.querySelector('#timein');
-  const timeOut = offerTime.querySelector('#timeout');
-  if (evt.target === timeIn) {
-    timeOut.value = evt.target.value;
-  } else if (evt.target === timeOut) {
-    timeIn.value = evt.target.value;
-  }
-};
+
+const checkTimeValidity = (timeValue) => {timeIn.value = timeValue.target.value; timeOut.value = timeValue.target.value;};
 
 export const checkValidity = () => {
-  //offerPrice.addEventListener('change', checkPriceValidity);
   offerPrice.addEventListener('change', () =>
     checkPriceValidity(),
   );
   offerTitleInput.addEventListener('input', checkTitleValidity);
-  //selectRooms.addEventListener('input', checkRoomNumberCapacityValidity);
   selectRooms.addEventListener('input', () =>
     checkRoomNumberCapacityValidity(),
   );
-  //offerType.addEventListener('change', checkPriceValidity);
   offerType.addEventListener('change', () =>
     checkPriceValidity(),
   );
-  offerTime.addEventListener('change', checkTimeValidity);
-  //capacitySelect.addEventListener('change', checkRoomNumberCapacityValidity);
-
+  timeIn.addEventListener('change', checkTimeValidity);
+  timeOut.addEventListener('change', checkTimeValidity);
   capacitySelect.addEventListener('change', () =>
     checkRoomNumberCapacityValidity(),
   );
