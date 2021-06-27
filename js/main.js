83e39ceb40357adcea95a6ddce3c0d36d2774dd7
Inq082 <smail_2001@rambler.ru>
2021-06-27 19:12:41 +0300

    8.11.v2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
-/*import {createListOffers, listOffers} from './data.js';
-
-createListOffers();
-window.console.log(listOffers);*/
 import {listOffers} from './data.js';
 import  {getCardTemplate} from './card.js';
+import {checkValidity} from './form.js';
 
 const cardTemplate = getCardTemplate(listOffers[0]);
 
 const mapCanvas = document.querySelector('#map-canvas');
 
 mapCanvas.appendChild(cardTemplate);
-//mapCanvas.classList.remove('hidden');
 window.console.log(cardTemplate);
 window.console.log(listOffers);
 
+checkValidity();
 
