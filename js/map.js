00158cd2a621c0afccfed4c7781ccea5a02e7eaa
Inq082 <smail_2001@rambler.ru>
2021-07-21 18:35:55 +0300

    поправки

--- js/map.js
+++ js/map.js
@@ -58,7 +58,7 @@ const addMarkers = (item) => {
 };
 const setTitleLayer = () => {
   L.tileLayer(
-      'https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+    'https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
       attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       minZoom: 0,
       maxZoom: 22,
